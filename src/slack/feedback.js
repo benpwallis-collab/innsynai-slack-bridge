2b3ce14886206d24f0bee826032cf3f982890839
benpwallis-collab <benpwallis@gmail.com>
2025-12-07 11:19:22 +0100

    Update feedback.js

--- src/slack/feedback.js
+++ src/slack/feedback.js
@@ -1,8 +1,13 @@
 export default function registerFeedback(app) {
+  console.log("📝 Feedback handlers registered");
+
   app.action("feedback_up", async ({ ack }) => {
     await ack();
+    console.log("👍 Feedback up");
   });
+
   app.action("feedback_down", async ({ ack }) => {
     await ack();
+    console.log("👎 Feedback down");
   });
 }
